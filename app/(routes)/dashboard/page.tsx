c2a7dfa20e8a48375321f23159b7ef3d5a981d4b
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-17 22:43:04 +0000

    authentication

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,8 +1,15 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { LogoutLink } from '@kinde-oss/kinde-auth-nextjs'
 import React from 'react'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+      <Button>
+        <LogoutLink>Logout</LogoutLink>
+        </Button>
+    </div>
   )
 }
 
