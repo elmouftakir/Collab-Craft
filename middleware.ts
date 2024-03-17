0d356e772b55afcf62684fecf7d8ba6717c18d4c
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-17 22:44:00 +0000

    authentication

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ export async function middleware(request: NextRequest) {
     const { isAuthenticated } = getKindeServerSession();
     if(await isAuthenticated)
     {
-      return NextResponse.redirect(new URL('/api/auth/login?post_login_redirect_url=/protected', request.url))
+      return NextResponse.redirect(new URL('/api/auth/login?post_login_redirect_url=/dashboard', request.url))
     }
 }
  
