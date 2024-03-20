580c64b3296c94131e578bc007b8becb78d4d84d
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-20 22:31:15 +0000

    database

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -10,7 +10,7 @@ import AdBanner from './../../_components/AdBanner'
 function Dashboard() {
 
   const convex=useConvex();
-  const {user}:any=useKindeBrowserClient();
+  const { user = {} }: any = useKindeBrowserClient();
   //const getUser=useQuery(api.user.getUser,{email:user?.email});
 
   const createUser=useMutation(api.user.createUser);
