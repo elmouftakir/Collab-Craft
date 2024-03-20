62a9b315b50f0e05d7503c0f833f6a9f6cc2878b
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-20 22:09:29 +0000

    database

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,6 +1,4 @@
 import { Button } from '@/components/ui/button';
 import { api } from '@/convex/_generated/api';
 import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
-import { useQuery } from 'convex/react';
-import React, { useEffect } from 'react';
-import { useClient } from 'next/client';
+import { useQuery } from 'convex/reaeClient } from 'next/client';
