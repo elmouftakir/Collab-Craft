736dd4e9c639ed53523bb64bed09296bda22bebe
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-20 22:32:23 +0000

    database

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -7,48 +7,46 @@ import React, { useEffect } from 'react'
 import Header from './_components/Header'
 import FileList from './_components/FileList'
 import AdBanner from './../../_components/AdBanner'
+
 function Dashboard() {
-
-  const convex=useConvex();
+  const convex = useConvex();
   const { user = {} }: any = useKindeBrowserClient();
-  //const getUser=useQuery(api.user.getUser,{email:user?.email});
 
-  const createUser=useMutation(api.user.createUser);
-  useEffect(()=>{
-      if(user)
-      {
-        checkUser()
-      }
-  },[user])
-  
+  const createUser = useMutation(api.user.createUser);
 
-  const checkUser=async()=>{
-    const result=await convex.query(api.user.getUser,{email:user?.email});
-    if(!result?.length)
-    {
-        createUser({
-          name:user.given_name,
-          email:user.email,
-          image:user.picture
-        }).then((resp)=>{
-          console.log(resp)
-        })
+  useEffect(() => {
+    if (user) {
+      checkUser();
     }
+  }, [user]);
 
+  const checkUser = async () => {
+    const result = await convex.query(api.user.getUser, { email: user?.email });
+    if (!result?.length) {
+      createUser({
+        name: user.given_name,
+        email: user.email,
+        image: user.picture
+      }).then((resp) => {
+        console.log(resp);
+      });
+    }
   }
+
+  // Uncomment this if you plan to use it later
+  // const getUser = useQuery(api.user.getUser, { email: user?.email });
+
   return (
     <div className='p-8'>
-      <Header/>
-
-      <FileList/>
+      <Header />
+      <FileList />
       <AdBanner
-          data-ad-slot="4796371341"
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
+        data-ad-slot="4796371341"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
     </div>
-
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
