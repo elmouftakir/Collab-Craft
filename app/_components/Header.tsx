86704953e139e8551b8c2c717513f753856798fc
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-20 20:39:24 +0000

    database

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -7,8 +7,8 @@ function Header() {
     <header className="bg-gray-900 dark:bg-gray-900">
   <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
   <Image src='/logo.png' alt='logo'
-  width={100}
-  height={100}
+  width={60}
+  height={60}
   />
 
     <div className="flex flex-1 items-center justify-end md:justify-between">
