f50eaa7c4aa1a37e792b46b5e3bd8eff78b752a9
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-21 17:42:09 +0000

    database

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -1,38 +1,24 @@
 "use client"
-import { Button } from '@/components/ui/button'
-import { api } from '@/convex/_generated/api'
-import { LogoutLink, useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs'
-import { useConvex, useMutation, useQuery } from 'convex/react'
-import React, { useEffect } from 'react'
+import { Button } from "@/components/ui/button"
+import { api } from "@/convex/_generated/api";
+import { LogoutLink, useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { useQuery } from "convex/react";
+import { useEffect } from "react";
 
 function Dashboard() {
-  const convex = useConvex();
-  const {user}:any=useKindeBrowserClient();
 
-  const getUser=useMutation(api.user.getUser);
+const {user}:any=useKindeBrowserClient();
+const getUser=useQuery(api.user.getUser,{email:user?.email})
+  useEffect(()=>{
 
-  useEffect(() => {
-    if (user) {
-      checkUser();
-    }
-  }, [user]);
-
-  const checkUser = async () => {
-    const result = await convex.query(api.user.getUser, { email: user?.email });
-    if (!result?.length) {
-      createUser({
-        name: user.given_name,
-        email: user.email,
-        image: user.picture
-      }).then((resp) => {
-        console.log(resp);
-      });
-    }
-  }
-
-  // Uncomment this if you plan to use it later
-  // const getUser = useQuery(api.user.getUser, { email: user?.email });
+  },[])
 
   return (
-
+    <div>
+      <Button>
+        <LogoutLink>logaut</LogoutLink>
+      </Button>
+    </div>
+  )
+}
 export default Dashboard;
