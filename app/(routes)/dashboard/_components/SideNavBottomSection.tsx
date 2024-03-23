a45c3d154946151984f4d576ea6a9922fb6dda4a
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-23 20:02:33 +0000

    dashboard

--- app/(routes)/dashboard/_components/SideNavBottomSection.tsx
+++ app/(routes)/dashboard/_components/SideNavBottomSection.tsx
@@ -80,7 +80,7 @@ function SideNavBottomSection({onFileCreate,totalFiles}:any) {
 
      
       
-      {/* Progress Bar  */}
+      {/* Progress Bar  *
       <div className='h-4 w-full bg-gray-200 rounded-full mt-5'>
           <div className={`h-4  bg-blue-600 rounded-full`}
           style={{ width: `${(totalFiles/5)*100}%` }}
@@ -90,7 +90,7 @@ function SideNavBottomSection({onFileCreate,totalFiles}:any) {
 
       <h2 className='text-[12px] mt-3'>
         <strong>{totalFiles}</strong> out of <strong>{Constant.MAX_FREE_FILE}</strong> files used</h2>
-      <h2 className='text-[12px] mt-1'>Upgrade your plan for unlimited access.</h2>  
+      <h2 className='text-[12px] mt-1'>Upgrade your plan for unlimited access.</h2>  */}
 
      </div>
   )
