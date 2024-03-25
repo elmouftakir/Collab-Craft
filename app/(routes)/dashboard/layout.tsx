a63184d299f1506258f47a4d7225ae6b1de2f28f
elmouftakir <abderahim.mouftakir@gmail.com>
2024-03-25 10:45:01 +0000

    update

--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -5,6 +5,7 @@ import { useConvex } from 'convex/react';
 import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react'
 import SideNav from './_components/SideNav';
+import { FileListContext } from '@/app/_context/FilesListContext';
 
 function DashboardLayout(
     {
@@ -31,7 +32,21 @@ function DashboardLayout(
         }
     }
 
-  return 
+  return (
+    <div>
+      <FileListContext.Provider value={{fileList_,setFileList_}}>
+      <div className='grid grid-cols-4'>
+          <div className='bg-white h-screen w-72 fixed'>
+          <SideNav/>
+          </div>
+          <div className='col-span-4 ml-72'>
+          {children}
+          </div>
+      </div>
+      </FileListContext.Provider>
+     
+      </div>
+  )
 }
 
 export default DashboardLayout
