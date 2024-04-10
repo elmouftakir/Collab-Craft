4f50012a4b8166672dcf3d2bf03f8922f4485981
elmouftakir <abderahim.mouftakir@gmail.com>
2024-04-10 19:20:33 +0000

    create chapter

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -9,6 +9,7 @@ import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
 import { AttachmentForm } from "./_components/attachment-form";
 import { PriceForm } from "./_components/price-form";
+import { ChaptersForm } from "./_components/chapters-form";
 
 
 
@@ -25,8 +26,14 @@ const CourseIdPage = async ({
     const  course = await db.course.findUnique({
         where:{
             id:params.courseId,
+            userId
         },
         include: {
+          chapters: {
+            orderBy: {
+              position: "asc",
+            },
+          },
           attachments: {
             orderBy: {
               createdAt: "desc",
@@ -50,6 +57,7 @@ const CourseIdPage = async ({
         course.imageUrl,
         course.price,
         course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
       ];
     
       const totalFields = requiredFields.length;
@@ -104,6 +112,10 @@ const CourseIdPage = async ({
                 Course chapters
               </h2>
             </div>
+            <ChaptersForm
+                initialData={course}
+                courseId={course.id}
+              />
           </div>
           <div>
             <div className="flex items-center gap-x-2">
